2c61e619ad21c8f8ceee97508aba9ff699895d7f
othman809 <othmanhassoun100@gmail.com>
2025-03-03 22:35:23 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from django.contrib import admin
 
 from django.contrib.auth import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,10 +13,7 @@ urlpatterns = [
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
+
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -23,3 +24,5 @@ urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('pages/', include(wagtail_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+path('blog/', include('blog.urls')),
+path('', include(wagtail_urls)),
